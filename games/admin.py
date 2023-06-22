160c2a07ff5c56d9c3fbdf21cf9837751f4b7202
farhaddanish <farhaddanish22@gmail.com>
2023-06-22 14:13:36 -0400

    games admin and models fixed

--- games/admin.py
+++ games/admin.py
@@ -1,7 +1,20 @@
 from django.contrib import admin
-
-# Register your models here.
-
-
 from .models import Games
-admin.site.register(Games)
+
+
+class GameAdmin (admin.ModelAdmin):
+    list_display = ("name", "type", "size", "date_added",)
+    list_display_links = ("name",)
+    list_filter = ("type", "name",)
+
+    readonly_fields = (
+        "date_added",
+        "date_updated",
+    )
+    ordering = ("-date_added",)
+
+    filter_horizontal = ()
+    exclude = ("size", "slug",)
+
+
+admin.site.register(Games, GameAdmin)
--- games/models.py
+++ games/models.py
@@ -1,19 +1,77 @@
 from django.db import models
-
+from django.core.exceptions import ValidationError
+import uuid
 # Create your models here.
 
 
+choices = (
+    ('FPS', 'First-Person Shooters (FPS)'),
+    ('TPS', 'Third-Person Shooters'),
+    ('Action-Adventure', 'Action-Adventure'),
+    ('Hack-Slash', 'Hack and Slash'),
+    ('Platformers', 'Platformers'),
+    ('Beat-em-up', 'Beat em Up'),
+    ('Open-World', 'Open-World Action'),
+    ('Battle-Royale', 'Battle Royale'),
+    ('RPG', 'Role-Playing Games (RPG)'),
+    ('Racing', 'Racing Games'),
+    ('Sports', 'Sports Games'),
+    ('Strategy', 'Strategy Games'),
+    ('Puzzle', 'Puzzle Games'),
+    ('Simulation', 'Simulation Games'),
+    ('Stealth', 'Stealth Games'),
+    ('Fighting', 'Fighting Games'),
+)
+
+
+video_formats = [
+    "MP4",
+    "AVI",
+    "MKV",
+    "MOV",
+    "WMV",
+    "FLV",
+    "WebM",
+    "MPEG",
+    "3GP",
+    "VOB",
+    "MPG",
+    "ASF",
+    "M4V",
+    "OGV",
+    "TS"
+]
+
+
+def validate_sample_video(value):
+    ext = value.name.split('.')[-1].upper()
+    if not ext.upper() in video_formats:
+        raise ValidationError(' Only support Video Files')
+
+
 class Games (models.Model):
+    uuid = models.UUIDField(
+        primary_key=True, default=uuid.uuid4, unique=True, editable=False)
     name = models.CharField(max_length=200, blank=False)
     describtions = models.TextField()
-    type = models.CharField(max_length=200, blank=False)
-    file = models.FileField(upload_to="gamesFile/games")
-    thumbnails = models.ImageField(upload_to="gamesFile/images")
-    sample_video = models.FileField(upload_to="gamesFile/sample_video")
-    size = models.IntegerField()
+    type = models.CharField(choices=choices, max_length=100)
+    file = models.FileField(upload_to="gamesFile/games", blank=False)
+    thumbnails = models.ImageField(upload_to="gamesFile/images", blank=False)
+    sample_video = models.FileField(
+        upload_to="gamesFile/sample_video", validators=[validate_sample_video], blank=False)
+    size = models.DecimalField(
+        max_digits=10, blank=True, null=True, decimal_places=2)
     date_added = models.DateField(auto_now_add=True)
     date_updated = models.DateField(auto_now=True)
-    slug = models.SlugField()
+    slug = models.SlugField(blank=False)
 
     def __str__(self):
         return self.name
+
+    def save(self, *args, **kwargs):
+        if self.pk:
+            self.size = self.file.size / (1000*1000*1000)
+        super().save(*args, **kwargs)
+
+    class Meta:
+        verbose_name_plural = "Games"
